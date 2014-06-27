ac7ba91bbbbd1b36ee9891800e6eb7349c00c126
a1985526 <yuahwen.cheng@gmail.com>
2014-06-27 19:24:49 +0800

    before json part

--- src/com/ywcheng1985/blogreader/MainListActivity.java
+++ src/com/ywcheng1985/blogreader/MainListActivity.java
@@ -6,14 +6,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
-import android.content.res.Resources;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.NetworkOnMainThreadException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 
@@ -25,39 +26,28 @@ public class MainListActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_list);
+		setContentView(R.layout.activity_main_list);		
 
-		try {
-			URL blogFeedUrl = new URL("http://rss.cnn.com/rss/edition.rss" + Number_of_Posts);
-			HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
-			connection.connect();
+		if(isNetworkAvailable()){
+			GetBlogPostsTask getBlogPostTask = new GetBlogPostsTask();
+			getBlogPostTask.execute();
 			
-			int reponseCode = connection.getResponseCode();
-			Log.i(TAG, "Response Code:" + reponseCode);
-		} 
-		catch (MalformedURLException e) {
-			Log.e(TAG,"Exception caught",e);
-		} 
-		catch (IOException e) {
-			Log.e(TAG,"Exception caught",e);
+		}else {
+			Toast.makeText(this, "Network is unavailabl!", Toast.LENGTH_LONG).show();
 		}
-		catch(NetworkOnMainThreadException e){
-			Log.e(TAG,"Exception caught",e);
-		}catch(Exception e){
-			Log.e(TAG,"Exception caught",e);
-		}
+		//Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
+
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 		
-
-		Resources resources = getResources();
-		mBlogPostTitles = resources.getStringArray(R.array.android_names);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, mBlogPostTitles);
-		setListAdapter(adapter);
-
-		//String message = getString(R.string.no_items);
-		//Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-
-		Button t = (Button) findViewById(R.id.useLogo);
-		t.setText(R.string.no_items);
+		boolean isAvailable = false;
+		if(networkInfo != null && networkInfo.isConnected()){
+			isAvailable = true;
+		}
+		return isAvailable;
 	}
 
 	@Override
@@ -73,5 +63,36 @@ public class MainListActivity extends ListActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private class GetBlogPostsTask extends AsyncTask<Object, Void, String>{
+
+		@Override
+		protected String doInBackground(Object... params) {
+			int responseCode = -1;
+
+			try {
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/" + Number_of_Posts);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+
+				responseCode = connection.getResponseCode();
+				Log.i(TAG, "Response Code:" + responseCode);
+			} 
+			catch (MalformedURLException e) {
+				Log.e(TAG,"Exception caught",e);
+			} 
+			catch (IOException e) {
+				Log.e(TAG,"Exception caught",e);
+			}
+			catch(Exception e){
+				Log.e(TAG,"Exception caught",e);
+			}
+
+
+			return "Code: " + responseCode;
+		}
+
+
 	}
 }
