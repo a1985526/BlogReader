489fa0d2f0460adbd65ba121412d60ec935caae2
a1985526 <yuahwen.cheng@gmail.com>
2014-06-27 16:52:43 +0800

    Wrong way to request data from the web

--- src/com/ywcheng1985/blogreader/MainListActivity.java
+++ src/com/ywcheng1985/blogreader/MainListActivity.java
@@ -1,51 +1,62 @@
 package com.ywcheng1985.blogreader;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainListActivity extends ListActivity {
 
-	protected String[] mAndroidNames = {
-			"Android beta",
-			"Android 1.0",
-			"Android 1.1",
-			"Cupcake",
-			"Donut"
-	};
+	protected String[] mBlogPostTitles;
+	public static final int Number_of_Posts = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 
-		ArrayAdapter<String> adapter = 
-				new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, mAndroidNames);
+		try {
+			URL blogFeedUrl = new URL("http://rss.cnn.com/rss/edition.rss" + Number_of_Posts);
+			HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+			connection.connect();
+		} 
+		catch (MalformedURLException e) {
+			Log.e(TAG,"Exception caught",e);
+		} catch (IOException e) {
+			Log.e(TAG,"Exception caught",e);
+		}
+
+		Resources resources = getResources();
+		mBlogPostTitles = resources.getStringArray(R.array.android_names);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, mBlogPostTitles);
 		setListAdapter(adapter);
 
+		//String message = getString(R.string.no_items);
+		//Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+		Button t = (Button) findViewById(R.id.useLogo);
+		t.setText(R.string.no_items);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
